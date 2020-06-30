db6f3cc2edf2f5bfb360ddbece58c005a6788406
kagim <kagimsim@163.com>
2020-06-30 15:15:56 +0800

    create some stars

--- pages/mine/mineMain/mineMain.js
+++ pages/mine/mineMain/mineMain.js
@@ -5,7 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+		startY: 0,     //滑动开始y轴位置
+		lastY: 0,     //滑动开始y轴位置
+		stars:{
+			height:0,
+			htmlStart:"<div style='position:relative;top:0px;left:0px;width:100vw;height:0px;display:block;box-sizing: border-box;background:#B8789D;'>",
+			html:"",
+			htmlEnd: "</div>"
+		}
   },
 
   /**
@@ -13,54 +20,50 @@ Page({
    */
   onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+	},
+	/**
+	 * 下拉刷新成功 （页面回弹）清空新生星星
+	 */
+	onPullDownRefresh() {
+		let stars = this.data.stars;
+		stars.htmlStart = "<div style='position:relative;top:0px;left:0px;width:100vw;height:0px;display:block;box-sizing: border-box;background:#B8789D;'>";
+		stars.html="";
+		stars.height = 0;
+		this.setData({
+			stars
+		})
+		wx.stopPullDownRefresh()
+	},
+	// 监听页面滚动事件 - 持续滚动函数
+	handletouchmove(event) {
+		// console.log(event)
+		let currentY = event.changedTouches[0].clientY
+		// console.log(currentY)
+		// console.log(this.data.startY)
+		if (currentY > this.data.startY) {
+			// 下拉时函数
+			let stars = this.data.stars;
+			let top = this.getNum(10, 190); // 距离顶部位置
+			let left = this.getNum(1, 99); // 距离左侧位置
+			let font = this.getNum(8,12); // 点的字体大小
+			if (stars.height<=50){
+				stars.htmlStart = "<div style='position:relative;top:0px;left:0px;width:100vw;height:" + (stars.height += 1) + "px;display:block;box-sizing: border-box;background:#B8789D;'>";
+			}else{
+				stars.htmlStart = "<div style='position:relative;top:0px;left:0px;width:100vw;height:50px;display:block;box-sizing: border-box;background:#B8789D;'>";
+			}
+			
+			stars.html += "<span style='position: absolute; top: " + top + "px; left: " + left + "vw;font-size:" + font +"px;color:#dedede;'>.</span>";
+			this.setData({
+				stars
+			})
+		}
+	},
+	//滑动开始事件-开始瞬间函数
+	handletouchstart: function (event) {
+		this.data.startY = event.changedTouches[0].clientY
+		//在这里获取 手 落点 距离event的位置 （应该是)
+	},
+	getNum(max,min){
+			return 	Math.floor(Math.random() * (max - min + 1) + min);
+	}
 })
